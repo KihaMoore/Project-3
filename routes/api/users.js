200b3f942c4d2ad5fe6dea9fce48b6679648d2c9
Kiha Moore <kihainusa@gmail.com>
2019-07-10 22:20:48 -0400

    User register validation added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,47 @@
 const express = require('express');
 const router = express.Router();
 
-const {check, validationResult} = require('express-validator/check')
+const {check, validationResult} = require('express-validator')
+
 
 //2route  POST api/users
 //2desc   Register user
 //@access Publicトークン要らない
-router.post('/', (req,res) => {
-    //このルートに送られてくるオブジェクトが見たい
+router.post('/', [
+    check('name', 'Name is required')
+    .not()
+    .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+        'password',
+        'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6})
+],
+  (req,res) => {
+      const errors = validationResult(req);
+      if(!errors.isEmpty()){
+          return res.status(400).json({errors:errors.array() });
+      }
+
+
+   //see if user exist
+
+ 
+
+   //Get users gravatar
+
+
+
+   //Encrypt password
+
+
+
+   //Rerurn jsonwebtoken
+   
+   
+   
+   
+      //このルートに送られてくるオブジェクトが見たい
     console.log(req.body);
 　　　res.send('user route');
 });   
